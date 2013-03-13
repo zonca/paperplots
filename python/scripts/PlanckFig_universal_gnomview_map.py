de39f40fc8c2d591ecfa325466575f11f1eca6c3
Andrea Zonca <code@andreazonca.com>
2013-03-13 11:26:16 -0700

    simplified gnomview example

--- python/scripts/PlanckFig_universal_gnomview_map.py
+++ python/scripts/PlanckFig_universal_gnomview_map.py
@@ -1,17 +1,5 @@
 import healpy as hp
-import numpy as np
 from planckcolors import planck_universal_cmap
-
-# 10^ formatter
-from matplotlib import ticker
-def format_func(x, pos):
-    if np.abs(x) < 90:
-        formatted = "%d" % x
-    else:
-        formatted = "%d^{%d}" % (10*np.sign(x), int(np.log10(np.abs(x))))
-    out = r"$%s$" % formatted 
-    return out 
-formatter = ticker.FuncFormatter(format_func)
 
 m = hp.ma(hp.read_map("../../data/wmap_band_iqumap_r9_7yr_W_v4.fits", 0)) * 1e3 # muK
 nside = hp.npix2nside(len(m))
