76f2718288a2050c55e37ca32f585db5aa6f0578
Andrea Zonca <code@andreazonca.com>
2014-11-21 11:37:13 -0800

    raw string for python 3

--- python/scripts/setup_matplotlib.py
+++ python/scripts/setup_matplotlib.py
@@ -17,7 +17,7 @@ params = {'backend': 'pdf',
           'font.sans-serif':'FreeSans'}
 
 # use of Sans Serif also in math mode
-rc('text.latex', preamble='\usepackage{sfmath}')
+rc('text.latex', preamble=r'\usepackage{sfmath}')
 
 rcParams.update(params)
 
